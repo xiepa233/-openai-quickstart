4b9bf460d0599165a2377c6339a9226191ebf957
xiepa <xiepa@oocl.com>
2024-07-26 14:17:37 +0800

    feat: Day 2 homework

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -25,3 +25,5 @@ if __name__ == "__main__":
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
     translator.translate_pdf(pdf_file_path, file_format)
+    translator.translate_pdf(pdf_file_path, file_format ,target_language="俄语")
+    translator.translate_pdf(pdf_file_path, file_format ,target_language="法语")
